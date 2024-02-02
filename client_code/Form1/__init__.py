f7cf5f88868eefba44451852c46e2bd871c19b66
karmstrong2@cclind.com <karmstrong2@cclind.com>
2024-02-02 20:19:15 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -14,9 +14,10 @@ class Form1(Form1Template):
     # Any code you write here will run before the form opens.  
     #anvil.users.login_with_form()
     
-  item_list = []
-  #for row in list(app_tables.costdata.search()):
-  #   item_list.append((row["itemNumber"], row))
-
-  #drop_down_cost_items.items = item_list
+item_list = []
+for row in list(app_tables.costdata.search()):
+    item_list.append((row["itemNumber"], row))
+    print((row["itemNumber"], row))
+  
+#drop_down_cost_items.items = item_list
   #self.drop_down_cost_items.items = [(row["itemNumber"], row) for row in app_tables.costdata.search()]
