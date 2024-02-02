8d876f37108c4c64ddb8f9e8e48fd92e006fb59b
karmstrong2@cclind.com <karmstrong2@cclind.com>
2024-02-02 22:16:07 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -5,6 +5,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
+
+
+
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -13,11 +16,15 @@ class Form1(Form1Template):
 
     # Any code you write here will run before the form opens.  
     #anvil.users.login_with_form()
-    
+
 item_list = []
 for row in list(app_tables.costdata.search()):
     item_list.append((row["itemNumber"], row))
-    print((row["itemNumber"], row))
+   
+Form1().drop_down_cost_items.items = item_list
+print(item_list.count("900-005811"))
+
+    
   
-#drop_down_cost_items.items = item_list
+
   #self.drop_down_cost_items.items = [(row["itemNumber"], row) for row in app_tables.costdata.search()]
