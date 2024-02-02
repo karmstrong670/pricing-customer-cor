ec64c3a54d96674d0eba401be01580676d4f761e
karmstrong2@cclind.com <karmstrong2@cclind.com>
2024-02-02 17:58:01 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -8,11 +9,14 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    
 
-    # Any code you write here will run before the form opens.
-  item_list = []
-  for row in app_tables.costdata.search():
-      item_list.append((row["itemNumber"], row))
+    # Any code you write here will run before the form opens.  
+    anvil.users.login_with_form()
+    print(f"This user has logged in: {anvil.users.get_user()['email']}")
+  #item_list = []
+  #for row in app_tables.costdata.search():
+ #    item_list.append((row["itemNumber"], row))
 
-  drop_down_cost_items.items = item_list
+  #drop_down_cost_items.items = item_list
   #self.drop_down_cost_items.items = [(row["itemNumber"], row) for row in app_tables.costdata.search()]
