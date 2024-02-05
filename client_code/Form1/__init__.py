cdbb1821fcb0dd282d576651738f0d66459db027
karmstrong2@cclind.com <karmstrong2@cclind.com>
2024-02-05 23:12:20 +0000

    Created UpdateCostTable; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,9 +1,11 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import pandas as pd
 
 
 
---
+++ server_code/UpdateCostTable.py
@@ -0,0 +1,34 @@
+import anvil.files
+from anvil.files import data_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+import pandas as pd 
+
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+
+@anvil.server.callable
+def import_excel_data(file):
+  with open(file, "rb") as f:
+    df = pd.read_excel(f)
+    for d in df.to_dict(orient="records"):
+      # d is now a dict of {columnname -> value} for this row
+      # We use Python's **kwargs syntax to pass the whole dict as
+      # keyword arguments
+      app_tables.costdata.add_row(**d)
+
+  #import_excel_data("AnvilTestFile.xlsx")
