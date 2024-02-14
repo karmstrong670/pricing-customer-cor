9a3cfaf9c633590818638f204bb91d66bb5a1378
karmstrong2@cclind.com <karmstrong2@cclind.com>
2024-02-14 01:34:22 +0000

    Added code to handle if one item is selected from the start auto fill other columns.

--- client_code/MainPage/__init__.py
+++ client_code/MainPage/__init__.py
@@ -26,17 +26,35 @@ class MainPage(MainPageTemplate):
     """This method is called when the user presses Enter in this text box"""
     "need to add distinct search"
     items = []
+    prices = []
+    
+    # resets dropdown for qty and price text
+    
+    self.drop_down_cost_uofm.items = items
+    self.label_price_final.text = "" 
     
     self.drop_down_cost_items.items = set((row["itemNumber"]) for row in app_tables.costdata.search(
       tables.order_by("itemNumber"),
       itemNumber=q.like(''+self.txtboxItem.text+'%')))
-   
-    # resets dropdown for qty and price text
-    self.drop_down_cost_uofm.items = items
-    self.label_price_final.text = "" 
+    #clears selected value
+    self.drop_down_cost_items.selected_value = None
     
-    if len(items) == 1 :
-      self.drop_down_cost_items.selected_value = items[0]
+    # if one item set item field and get data for unit of measure
+    if len(self.drop_down_cost_items.items) == 1 :
+      self.drop_down_cost_items.selected_value = self.txtboxItem.text
+
+      self.drop_down_cost_uofm.items = set((row["unitOfMeasure"]) for row in app_tables.costdata.search(
+        itemNumber=self.drop_down_cost_items.selected_value) )
+      self.label_price_final.text = ""
+    # checks if one unit of measure 
+    if len(self.drop_down_cost_uofm.items) == 1:
+      self.drop_down_cost_uofm.selected_value = self.drop_down_cost_uofm.items[0]
+      
+      prices = [(row["price"]) for row in app_tables.costdata.search(
+      itemNumber=self.drop_down_cost_items.selected_value,
+      unitOfMeasure= self.drop_down_cost_uofm.selected_value ) ]
+      self.label_price_final.text = prices[0]
+
     
     pass
   
@@ -45,6 +63,8 @@ class MainPage(MainPageTemplate):
     #print(self.drop_down_cost_items.selected_value)
     self.drop_down_cost_uofm.items = set((row["unitOfMeasure"]) for row in app_tables.costdata.search(
       itemNumber=self.drop_down_cost_items.selected_value) )
+    #clears selected value
+    self.drop_down_cost_uofm.selected_value = None
     self.label_price_final.text = "" #resets price text 
     pass
 
